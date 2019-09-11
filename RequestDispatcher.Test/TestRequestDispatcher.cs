c0cb43fbdeaa135cb98baa1eff68209a872a743e
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 22:13:37 +0200

    Add TestRequestDispatcher

---
+++ RequestDispatcher.Test/TestRequestDispatcher.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestDispatcher.Test
+{
+    class TestRequestDispatcher : BaseRequestDispatcher
+    {
+        public int SetHeadersCallCount { get; private set; } = 0;
+        public HttpRequestMessage LastSetHeadersMessage { get; private set; }
+
+        public int OnErrorResponseCallCount { get; private set; } = 0;
+        public HttpResponseMessage LastOnErrorResponseMessage { get; private set; }
+
+        public TestRequestDispatcher(HttpClient httpClient) : base(httpClient)
+        {
+
+        }
+
+        protected override Task OnErrorResponse(HttpResponseMessage response)
+        {
+            OnErrorResponseCallCount++;
+            LastOnErrorResponseMessage = response;
+
+            return base.OnErrorResponse(response);
+        }
+
+        protected override void SetHeaders(HttpRequestMessage request)
+        {
+            SetHeadersCallCount++;
+            LastSetHeadersMessage = request;
+
+            base.SetHeaders(request);
+        }
+    }
+}
