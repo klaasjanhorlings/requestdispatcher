33e3298249e8bfe37edfa5b44fb3adeb90aa0ffa
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 18:46:39 +0200

    Implement TestHttpMessageHandler

--- RequestDispatcher.Test/TestHttpMessageHandler.cs
+++ RequestDispatcher.Test/TestHttpMessageHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -9,9 +10,18 @@ namespace RequestDispatcher.Test
 {
     public class TestHttpMessageHandler : HttpMessageHandler
     {
+        public int CallCount { get; private set; } = 0;
+        public HttpRequestMessage LastRequestMessage { get; private set; }
+        public HttpResponseMessage LastResponseMessage { get; private set; }
+
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            CallCount++;
+
+            LastRequestMessage = request;
+            LastResponseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return Task.FromResult(LastResponseMessage);
         }
     }
 }
