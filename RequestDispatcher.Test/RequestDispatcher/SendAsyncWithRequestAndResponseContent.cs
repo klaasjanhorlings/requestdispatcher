0926c3b9f1970f1c4312ec4ee877b0ed1e905520
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 21:26:30 +0200

    Add SendAsyncWithRequestAndResponseContent test class

--- RequestDispatcher.Test/RequestDispatcher/SendAsyncWithRequestAndResponseContent.cs
+++ RequestDispatcher.Test/RequestDispatcher/SendAsyncWithRequestAndResponseContent.cs
@@ -9,9 +9,21 @@ using System.Threading.Tasks;
 namespace RequestDispatcher.Test.RequestDispatcher
 {
     [TestClass]
-    public class SendAsyncWithRequestAndResponseContent : SendBase
+    public class SendAsyncWithRequestAndResponseContent : SendContentBase
     {
         protected override async Task CallSend(HttpMethod method, string path, CancellationToken token = default)
             => sendResult = await requestDispatcher.SendAsync<object, object>(method, path, requestContent, token);
+
+        [TestMethod]
+        public override async Task CallsSerializeWithPassedContent() => await base.CallsSerializeWithPassedContent();
+
+        [TestMethod]
+        public override async Task SetsSerializedContentToRequestMessage() => await base.SetsSerializedContentToRequestMessage();
+
+        [TestMethod]
+        public override async Task CallsDeserializeWithReturnedContent() => await base.CallsDeserializeWithReturnedContent();
+
+        [TestMethod]
+        public override async Task ReturnsDeserializedResult() => await base.ReturnsDeserializedResult();
     }
 }
