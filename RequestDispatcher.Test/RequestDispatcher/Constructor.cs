da48b629e766252efeef9c8938574fbc9cc9c9a1
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 17:20:59 +0200

    Add MSTest project and initial test class

---
+++ RequestDispatcher.Test/RequestDispatcher/Constructor.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RequestDispatcher.Test.RequestDispatcher
+{
+    [TestClass]
+    public class Constructor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
