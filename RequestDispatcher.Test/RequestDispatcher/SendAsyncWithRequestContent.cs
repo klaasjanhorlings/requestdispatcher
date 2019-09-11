03bdc86ae6c87b40e63bf9a0887f86b9b0bcb4e2
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 19:36:16 +0200

    Add SendAsyncWithRequestContent test class

---
+++ RequestDispatcher.Test/RequestDispatcher/SendAsyncWithRequestContent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestDispatcher.Test.RequestDispatcher
+{
+    [TestClass]
+    public class SendAsyncWithRequestContent
+    {
+    }
+}
