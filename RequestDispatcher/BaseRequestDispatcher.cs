40a596802b353017b214245e9eaeeba9541d3b08
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 17:20:47 +0200

    Add BaseRequestDispatcher class implementing IRequestDispatcher

---
+++ RequestDispatcher/BaseRequestDispatcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequestDispatcher
+{
+    public class BaseRequestDispatcher : IRequestDispatcher
+    {
+        public Task SendAsync(HttpMethod method, string path, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendAsync<TRequest>(HttpMethod method, string path, TRequest body, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> SendAsync<TResponse>(HttpMethod method, string path, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TResponse> SendAsync<TResponse, TRequest>(HttpMethod method, string path, TRequest body, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
