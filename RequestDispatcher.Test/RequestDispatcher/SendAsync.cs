21f0a2264130e0e42f337421d4296c988d9e8f3a
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 18:16:44 +0200

    Add first test for SendAsync call testing passed HttpMethod

---
+++ RequestDispatcher.Test/RequestDispatcher/SendAsync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace RequestDispatcher.Test.RequestDispatcher
+{
+    [TestClass]
+    public class SendAsync
+    {
+        [TestMethod]
+        public void SetsHttpMethodToRequestMessage()
+        {
+            var httpClient = new HttpClient();
+            var requestDispatcher = new BaseRequestDispatcher(httpClient);
+            requestDispatcher.SendAsync(HttpMethod.Get, "http://localhost");
+
+            // Check HttpClient.Send call arguments??
+        }
+    }
+}
