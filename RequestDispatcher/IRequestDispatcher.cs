24e01e23747a6be2487898a109fad1ddf5217331
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 17:08:17 +0200

    Initial commit

---
+++ RequestDispatcher/IRequestDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequestDispatcher
+{
+    public interface IRequestDispatcher
+    {
+        Task SendAsync(HttpMethod method, string path, CancellationToken cancellationToken = default(CancellationToken));
+        Task SendAsync<TRequest>(HttpMethod method, string path, TRequest body, CancellationToken cancellationToken = default(CancellationToken));
+
+        Task<TResponse> SendAsync<TResponse>(HttpMethod method, string path, CancellationToken cancellationToken = default(CancellationToken));
+        Task<TResponse> SendAsync<TResponse, TRequest>(HttpMethod method, string path, TRequest body, CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
