1ab6c4550184b37ef77c3d3168ca4c1372cd7999
Klaasjan Horlings <k.horlings@gmail.com>
2019-09-11 20:30:48 +0200

    Add SendAsyncWithResponseContent test class

---
+++ RequestDispatcher.Test/RequestDispatcher/SendAsyncWithResponseContent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequestDispatcher.Test.RequestDispatcher
+{
+    [TestClass]
+    public class SendAsyncWithResponseContent : SendBase
+    {
+        protected override async Task CallSend(HttpMethod method, string path, CancellationToken token = default)
+            => await requestDispatcher.SendAsync<object>(method, path, token);
+    }
+}
